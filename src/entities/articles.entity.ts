47d37acf56f61aa1410e69df9cf692693a586fc7
JooYoung2274 <jooooo2274@gmail.com>
2023-06-14 10:11:04 +0900

    entity 업데이트

---
+++ src/entities/articles.entity.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Comments } from "./comments.entity";
+import { Boards } from "./boards.entity";
+
+@Entity("Articles")
+export class Articles {
+  @PrimaryGeneratedColumn({ type: "int", name: "id" })
+  id: number;
+
+  @Column("varchar", { name: "title" })
+  title: string;
+
+  @Column("varchar", { name: "content" })
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @OneToMany(() => Comments, (comments) => comments.Articles)
+  Comments: Comments[];
+
+  @Column("int", { name: "BoardId", nullable: true })
+  BoardId: number;
+
+  @ManyToOne(() => Boards, (Board) => Board.Articles, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  })
+  @JoinColumn([{ name: "BoardId", referencedColumnName: "id" }])
+  Boards: Boards;
+}
---
+++ src/entities/comments.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Articles } from "./articles.entity";
+
+@Entity("Comments")
+export class Comments {
+  @PrimaryGeneratedColumn({ type: "int", name: "id" })
+  id: number;
+
+  @Column("varchar", { name: "content" })
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column("int", { name: "ArticleId", nullable: true })
+  ArticleId: number;
+
+  @ManyToOne(() => Articles, (Article) => Article.Comments, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  })
+  @JoinColumn([{ name: "ArticleId", referencedColumnName: "id" }])
+  Articles: Articles;
+}
